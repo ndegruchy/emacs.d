f27102789bca9f2711a6ebb86b534e504ff41ff8
Nathaniel Robert DeGruchy <nathan@degruchy.org>
2014-10-29 11:42:22 -0400

    BS whitespace 'fixes'

--- init.el
+++ init.el
@@ -65,12 +65,11 @@
 (require 'package)
 (package-initialize)
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
-						 ("org" . "http://orgmode.org/elpa/")
-						 )
-	  )
+(setq package-archives '(("gnu"			. "http://elpa.gnu.org/packages/")
+                         ("marmalade"	. "http://marmalade-repo.org/packages/")
+                         ("melpa"		. "http://melpa.milkbox.net/packages/")
+						 ("org"			. "http://orgmode.org/elpa/")
+						 ))
 
 ;; Evil
 (require 'evil)
