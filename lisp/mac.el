0a7c5e3cbe09680a6a1e1e88cc758264fee27868
Nathaniel Robert DeGruchy <nathan@degruchy.org>
2016-06-21 22:10:24 -0400

    Remove unneeded file

--- lisp/mac.el
+++
@@ -1,7 +0,0 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Mac-specific settings ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Window Geometry
-(add-to-list 'default-frame-alist '(width  . 100))
-(add-to-list 'default-frame-alist '(height . 34))
