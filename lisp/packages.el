7722eefa482e4d013a696453636f39b37e607739
Nathan DeGruchy <ndegruchy@nathan-ccat.local>
2016-06-27 08:56:50 -0400

    Add smex initialize

--- lisp/packages.el
+++ lisp/packages.el
@@ -128,6 +128,8 @@
 
 (use-package smex
   :ensure t
+  :config
+  (smex-initialize)
   :bind (("M-x" . smex)
          ("M-X" . smex-major-mode-commands)
          ("C-c C-c M-x" . execute-extended-command)))
