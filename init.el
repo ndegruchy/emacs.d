56c1bcaec34dd4b2c64cbe5a0cd8f73641e4f75b
Nathan DeGruchy <ndegruchy@nathan-ccat.local>
2016-07-18 11:07:21 -0400

    New platform-specific modules for customizations

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; Nathan's Emacs File
 ;; Now with less Cider
-;; Time-stamp: <2016-06-21 22:07:58 ndegruchy>
+;; Time-stamp: <2016-07-18 11:06:04 ndegruchy>
 
 ;; Load the local lisp directory
 (add-to-list 'load-path "~/.emacs.d/lisp")
@@ -14,3 +14,14 @@
 
 (when (file-exists-p custom-file)
   (load custom-file)) ;; For custom.el
+
+;; Load platform-specific customizations
+
+(when (eq system-type 'darwin)
+  (load-library "mac"))
+
+(when (eq system-type 'windows-nt)
+  (load-library "windows"))
+
+(when (eq system-type 'gnu/linux)
+  (load-library "linux"))
