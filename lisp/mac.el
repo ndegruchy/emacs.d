96e96e24f0b75a178cdfcdc37e24abebc7f23340
Nathan DeGruchy <ndegruchy@nathan-ccat.local>
2016-07-20 17:58:29 -0400

    Mac keybindings

--- lisp/mac.el
+++ lisp/mac.el
@@ -1,7 +1,11 @@
 ;; Mac customizations
 
-;; Bindings
-(global-set-key (kbd "s-s") 'save-buffer)
+;; Unbind
+(global-unset-key (kbd "s-x"))
+
+;; Binding
+(global-set-key   (kbd "s-s") 'save-buffer)
+(global-set-key   (kbd "s-x") 'smex)
 
 ;; Packages
 (use-package exec-path-from-shell
