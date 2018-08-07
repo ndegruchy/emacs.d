fb75848d753ffb0cef624c01797d32389f27b356
Nathan DeGruchy <nathan@degruchy.org>
2018-08-07 13:54:37 -0400

    Unwrap some built-in packages from `use-package`

--- settings.d/included-packages.el
+++ settings.d/included-packages.el
@@ -1,18 +1,15 @@
 ;; included-packages.el
 ;; Configure packages distributed with Emacs
 
-(use-package ido
-  :ensure t
-  :config
-  (setq ido-enable-flex-matching t
-	ido-everywhere t
-	ido-vertical-define-keys 'C-n-C-p-up-and-down)
-  (ido-mode 1)
-  (ido-vertical-mode 1))
+;; Ido
+(setq ido-enable-flex-matching t
+      ido-everywhere t
+      ido-vertical-define-keys 'C-n-C-p-up-and-down)
+(ido-mode 1)
+(ido-vertical-mode 1)
 
-(use-package uniquify
-  :config
-  (setq uniquify-buffer-name-style    'reverse
-        uniquify-separator            "/"
-        uniquify-after-kill-buffer-p  t
-        uniquify-ignore-buffers-re    "^\\*"))
+;; Uniquify
+(setq uniquify-buffer-name-style    'reverse
+      uniquify-separator            "/"
+      uniquify-after-kill-buffer-p  t
+      uniquify-ignore-buffers-re    "^\\*")
