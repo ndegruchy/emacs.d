519215329cab25f7537a78e4c2cb3bd150e50d21
Nathan DeGruchy <nathan@degruchy.org>
2018-12-08 14:01:23 -0500

    Remove magit and friends
    The built-in VC commands are generally all I need for my purposes.

--- custom-lisp.d/emacs-customize.el
+++ custom-lisp.d/emacs-customize.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ido-completing-read+ which-key use-package undo-tree tablist olivetti no-littering markdown-mode magit iedit ido-vertical-mode goto-chg fish-mode exec-path-from-shell embrace base16-theme auctex amx ace-window))))
+    (which-key use-package undo-tree no-littering markdown-mode ido-vertical-mode ido-completing-read+ fish-mode exec-path-from-shell embrace base16-theme auctex amx ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
