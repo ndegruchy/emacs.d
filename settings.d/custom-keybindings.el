34ae5776462fd126c804385a9d9bf5736e5b2d66
Nathan DeGruchy <nathan@degruchy.org>
2018-12-28 17:00:20 -0500

    Avy mode replaces ace

--- settings.d/custom-keybindings.el
+++ settings.d/custom-keybindings.el
@@ -11,7 +11,9 @@
  ("C-c E"  . embrace-commander)
  ("C-c e"  . embrace-add)
  ("C-c s"  . er/expand-region)
- ("C-c j"  . ace-jump-mode)
+ ("C-c j"  . avy-goto-char)
+ ("C-c J"  . avy-goto-word-0)
+ ("C-c l"  . avy-goto-line)
  ("C-c -"  . split-window-vertically)
  ("C-c |"  . split-window-horizontally))
 
