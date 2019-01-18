7d68e086013ca6047bb7032cae514a491e115285
Nathan DeGruchy <nathan@degruchy.org>
2019-01-17 22:14:36 -0500

    Remove proselint

--- settings.d/required-packages.el
+++ settings.d/required-packages.el
@@ -48,18 +48,7 @@
   :ensure t)
 
 (use-package flycheck
-  :ensure t
-  :config
-  (flycheck-define-checker proselint
-    "A linter for prose."
-    :command ("proselint" source-inplace)
-    :error-patterns
-    ((warning line-start (file-name) ":" line ":" column ": "
-              (id (one-or-more (not (any " "))))
-              (message) line-end))
-    :modes (text-mode markdown-mode gfm-mode message-mode latex-mode))
-  
-  (add-to-list 'flycheck-checkers 'proselint))
+  :ensure t)
 
 (use-package ido-vertical-mode
   :ensure t)
