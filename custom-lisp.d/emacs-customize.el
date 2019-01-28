50019078091dd10d57dbe5cd33e21eb2bb2236a5
Nathan DeGruchy <ndegruchy@ndegruchy-chbk.degruchy.org>
2019-01-28 18:58:02 -0500

    Remove emacs customize, it changes too much

--- custom-lisp.d/emacs-customize.el
+++ custom-lisp.d/emacs-customize.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (windresize which-key use-package pkg-info no-littering ido-vertical-mode fish-mode exec-path-from-shell embrace dired-rsync dired-atool auctex))))
+    (windresize which-key use-package pkg-info no-littering ido-vertical-mode fish-mode exec-path-from-shell embrace dired-rsync dired-atool))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
