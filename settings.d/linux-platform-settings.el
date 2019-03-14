793dd49a3c767cbe51a2a1b73de3b81dd26867cb
Nathan DeGruchy <nathan@degruchy.org>
2019-03-13 20:30:46 -0400

    Add emms
    Yeah, we're back. Less MPD this time.

--- settings.d/linux-platform-settings.el
+++ settings.d/linux-platform-settings.el
@@ -24,6 +24,22 @@
 
 (when (string= (system-name) "ndegruchy-dt.degruchy.org")
   ;; Only load on my desktop, please.
+
+  (use-package emms
+    :config
+    (require 'emms-setup)
+    (require 'emms-info-libtag)
+    (require 'emms-source-file)
+    (emms-all)
+    (emms-default-players)
+    (setq emms-source-list                   '((emms-directory-tree "/mnt/ndegruchy/Music"))
+          emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find
+	  emms-info-functions                '(emms-info-libtag)
+	  emms-info-auto-update              t
+	  emms-info-asynchronously           t
+	  emms-browser-covers                'emms-browser-cache-thumbnail-async
+	  emms-player-list                   '(emms-player-vlc)))
+  
   (use-package tex-site
     :ensure auctex
     :config
