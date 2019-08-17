10c1c8ea66745591c4a813a4d73049a8266e32b3
Nathan DeGruchy <nathan@degruchy.org>
2019-08-17 13:44:00 -0400

    Remove old settings files

--- settings.d/windows-platform-settings.el
+++
@@ -1,6 +0,0 @@
-;; windows-platform-settings.el
-;; Set windows-platform setings here
-
-(add-to-list 'exec-path "C:/Programs Files (x86)/Aspell/bin")
-(add-to-list 'exec-path "C:/Git/bin")
-(setq ispell-program-name "aspell")
