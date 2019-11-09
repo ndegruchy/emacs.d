98cbd48add6708b77f1ad470190232f3728b323c
Nathan DeGruchy <nathan@degruchy.org>
2019-11-09 16:55:31 -0500

    Add markdown mode

--- settings.d/required-packages.el
+++ settings.d/required-packages.el
@@ -93,6 +93,14 @@
 (use-package ido-vertical-mode
   :ensure t)
 
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 (use-package muse
   :ensure t)
 
