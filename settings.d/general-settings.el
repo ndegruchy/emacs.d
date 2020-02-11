f5905025933ca6702ebf2154c802aaf1fb562207
Nathan DeGruchy <nathan@degruchy.org>
2020-02-11 18:53:54 -0500

    Add savehist mode

--- settings.d/general-settings.el
+++ settings.d/general-settings.el
@@ -37,7 +37,8 @@
       tooltip-mode                           nil
       show-paren-mode                        t
       default-tab-width                      4
-      x-gtk-use-system-tooltips              nil)
+      x-gtk-use-system-tooltips              nil
+      savehist-mode                          1)
 
 (tool-bar-mode       -1)
 (menu-bar-mode       -1)
