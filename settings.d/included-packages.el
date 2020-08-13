79fa637d73c13fe7f951f21c48f5c937d6e61e7d
Nathan DeGruchy <nathan@degruchy.org>
2020-08-13 10:38:55 -0400

    ERC stuff

--- settings.d/included-packages.el
+++ settings.d/included-packages.el
@@ -30,6 +30,36 @@
   (put 'dired-find-alternate-file 'disabled nil)
   :hook (dired-mode . dired-hide-details-mode))
 
+;; TODO: Update with SASL and don't save the password
+;;       in the source code here...
+;; (use-package erc
+;;   :config
+;;   (require 'erc-join)
+;;   (require 'erc-match)
+;;   (require 'erc-fill)
+;;   (erc-autojoin-mode t)
+;;   (erc-match-mode t)
+
+;;   (setq erc-autojoin-channels-alist '(("freenode.net"
+;; 									   "#emacs" "#indieweb" "#indieweb-chat" "#fsf"
+;; 									   "#fsf-members" "#sr.ht" "#debian"))
+;; 		erc-match-keywords '("degruchy" "ndegruchy" "nathan")
+;; 		erc-log-insert-log-on-open nil
+;; 		erc-log-channels nil
+;; 		erc-save-buffer-on-part nil
+;; 		erc-timestamp-only-if-changed-flag nil
+;;         erc-timestamp-format "%H:%M "
+;;         erc-fill-prefix "      "
+;;         erc-insert-timestamp-function 'erc-insert-timestamp-left
+;; 		erc-prompt-for-nickserv-password t)
+
+;;   (defun irc-maybe ()
+;; 	"Ask if we want to connect to IRC"
+;; 	(interactive)
+;; 	(when (y-or-n-p "Connec to IRC? ")
+;; 	  (erc :server "chat.freenode.net" :port 6697
+;; 		   :nick "ndegruchy" :full-name "Nathan DeGruchy"))))
+
 (use-package eshell
   :config
   (setq eshell-visual-commands '("less" "tmux" "htop" "top" "bash" "fish" "zsh")
