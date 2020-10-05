abb99735f3f8a13fce14bebd7e98d20e3039ee5c
Nathan DeGruchy <nathan@degruchy.org>
2020-10-05 10:20:38 -0400

    Emms and Circe

--- settings.d/required-packages.el
+++ settings.d/required-packages.el
@@ -44,18 +44,23 @@
 (use-package bind-key
   :after (use-package))
 
-
 (use-package circe
   :ensure t
   :bind ("C-c k" . circe)
-  :init
+  :config
   (setq circe-reduce-lurker-spam t
+		circe-default-directory "~/.cache/logs/"
+		circe-default-part-message "Goodbye"
+		circe-default-quit-message "Goodbye"
 		circe-network-options '(("Freenode"
-								 :tls t
+								 :host "chat.freenode.net"
+								 :port 7000
+								 :use-tls t
 								 :nick "ndegruchy"
 								 :sasl-username "ndegruchy"
-								 :sasl-password freenode-password
-								 :channels ("#emacs" "#debian" "#indieweb" "#indieweb-chat")))))
+								 :sasl-password ,freenode-password
+								 :channels ("#emacs" "#debian" "#indieweb" "#indieweb-chat"
+											"#kde" "#firefox")))))
 
 (use-package crontab-mode
   :load-path "~/.emacs.d/site-lisp.d/")
@@ -77,6 +82,20 @@
 (use-package emmet-mode
   :hook (web-mode css-mode sgml-mode)
   :ensure t)
+
+(use-package emms
+  :init
+  (require 'emms-setup)
+  (require 'emms-info-libtag)
+  (emms-all)
+  :config
+  (setq emms-source-file-default-directory "~/Music"
+		emms-info-asynchronously t
+		emms-info-functions '(emms-info-libtag)
+		emms-show-format "♪ %s")
+  (if (executable-find "cvlc")
+	  (setq emms-player-list '(emms-player-vlc))
+	(emms-default-players)))
 
 (use-package exec-path-from-shell
   :ensure t
@@ -116,9 +135,31 @@
   (add-to-list 'recentf-exclude no-littering-etc-directory))
 
 (use-package notmuch
-  :bind ("C-c n" . notmuch)
+  :bind (("C-c n" . notmuch)
+		 :map notmuch-tree-mode-map
+		 ("d" . nm/tree-delete)
+		 ("s" . nm/tree-sent)
+		 :map notmuch-search-mode-map
+		 ("d" . nm/search-delete)
+		 ("s" . nm/search-sent))
   :hook (message-send-hook . message-sign-encrypt-if-all-keys-available)
   :config
+  (defun nm/tree-delete()
+	"Deletes a message from tree view"
+	(interactive)
+	(notmuch-tree-tag (list "+delete" "+deleted" "-unread")))
+  (defun nm/tree-sent()
+	"Marks a message as sent"
+	(interactive)
+	(notmuch-tree-tag (list "+sent" "-inbox" "-unread")))
+  (defun nm/search-delete()
+	"Deletes a message from search view"
+	(interactive)
+	(notmuch-search-tag (list "+delete" "+deleted" "-unread")))
+  (defun nm/search-sent()
+	"Marks a message as sent"
+	(interactive)
+	(notmuch-search-tag (list "+sent" "-inbox" "-unread")))
   (setq mail-specify-envelope-from t
 		message-sendmail-envelope-from 'header
 		mail-envelope-from 'header
@@ -126,6 +167,8 @@
 		message-send-mail-function 'message-send-mail-with-sendmail
 		message-directory "~/.local/share/mail/drafts/"
 		notmuch-fcc-dirs "~/.local/share/mail/sent/"
+		notmuch-mua-compose-in 'new-frame
+		message-kill-buffer-on-exit t
 		notmuch-crypto-process-mime t
 		notmuch-saved-searches '((:name "inbox"
 										:key "i"
