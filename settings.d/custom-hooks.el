5f7959e096481013491dbb636a7f06fbd24c25e9
Nathan DeGruchy <nathan@degruchy.org>
2020-11-12 08:53:43 -0500

    Cleanup

--- settings.d/custom-hooks.el
+++ settings.d/custom-hooks.el
@@ -6,15 +6,3 @@
 
 ;; Protect buffers
 (add-hook 'after-init-hook #'protect-buffers)
-
-;; Emmet
-(add-hook 'sgml-mode-hook 'emmet-mode)
-(add-hook 'css-mode-hook 'emmet-mode)
-
-;; Markdown mode
-(add-hook 'markdown-mode 'flyspell-mode)
-(add-hook 'markdown-mode 'auto-fill-mode)
-
-;; Org mode
-(add-hook 'org-mode-hook 'turn-on-auto-fill)
-(add-hook 'org-mode-hook 'flyspell-mode)
