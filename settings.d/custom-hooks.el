4c5fffe5aded7fe2b8a5224046aca59e123c9222
Nathan DeGruchy <nathan@degruchy.org>
2020-11-22 13:13:00 -0500

    Removed

--- settings.d/custom-hooks.el
+++
@@ -1,8 +0,0 @@
-;; custom-hooks.el
-;; Set custom mode or setting hooks here
-
-;; Timestamps
-(add-hook 'before-save-hook 'time-stamp)
-
-;; Protect buffers
-(add-hook 'after-init-hook #'protect-buffers)
