f29394b979c3254a9f2844af5d47e10cb637eb03
Nathan DeGruchy <nathan@degruchy.org>
2022-01-31 13:39:18 -0500

    Update windows dired settings

--- settings.d/win32-settings.el
+++ settings.d/win32-settings.el
@@ -1,2 +1,6 @@
 (add-to-list 'default-frame-alist '(font . "Cascadia Code-14"))
-(setq default-directory "C:/Users/NathanRDeGruchy/")
+(setq default-directory "C:/Users/NathanRDeGruchy/"
+	  dired-listing-switches "-a -F -l"
+	  ls-lisp-emulation nil
+	  ls-lisp-dirs-first t
+	  ls-lisp-use-insert-directory-program nil)
