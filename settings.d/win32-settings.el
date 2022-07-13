4befb298b644690273dab854297f640b8dfbc5b5
Nathan DeGruchy <nathan@degruchy.org>
2022-07-13 13:38:48 -0400

    Clear out old code from w32 platform

--- settings.d/win32-settings.el
+++ settings.d/win32-settings.el
@@ -1,9 +1,3 @@
 (add-to-list 'default-frame-alist '(font . "Cascadia Code-14"))
 (setq default-directory "C:/Users/NathanRDeGruchy/"
-	  dired-listing-switches "-a -F -l"
-	  ls-lisp-emulation nil
-	  ls-lisp-dirs-first t
-	  ls-lisp-use-insert-directory-program nil)
-
-(add-to-list 'load-path (concat user-emacs-directory "site-lisp.d/"))
-(require 'ls-lisp+)
+	  dired-listing-switches "-a -F -l")
