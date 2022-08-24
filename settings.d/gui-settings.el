a2762d8ff69f68eaa9a1ef7892ae05a881a3f975
Nathan DeGruchy <nathan@degruchy.org>
2022-08-24 10:16:11 -0400

    Disable toolbar

--- settings.d/gui-settings.el
+++ settings.d/gui-settings.el
@@ -3,6 +3,7 @@
 (scroll-bar-mode -1)
 (tooltip-mode    -1)
 (tool-bar-mode   -1)
+(menu-bar-mode   -1)
 
 (setq x-gtk-use-system-tooltips nil
 	  inhibit-x-resources       t)
@@ -12,7 +13,7 @@
 ;; ones. Emacs doesn't seem to like them.
 ;; https://github.com/microsoft/cascadia-code/issues/589
 ;; (add-to-list 'default-frame-alist '(font . "Cascadia Code-15"))
-(add-to-list 'default-frame-alist '(font . "Iosevka-18"))
+(add-to-list 'default-frame-alist '(font . "Iosevka-17"))
 
 (set-face-attribute 'menu nil
 					:inverse-video nil
