40964581a4a5493bd00adbc2c52b8f4faa46ad75
Nathan DeGruchy <nathan@degruchy.org>
2022-08-24 15:44:41 -0400

    News feed!

---
+++ settings.d/news-feeds.el
@@ -0,0 +1,21 @@
+;;; news-feeds.el --- A set of configurations and articles for NewsTicker  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Nathan DeGruchy
+
+;; Author: Nathan DeGruchy <nathan@degruchy.org>
+;; Keywords: comm, news, hypermedia
+
+(use-package newsticker
+  :ensure nil
+  :custom
+  (setq newsticker-url-list
+		'(("NPR News"                "https://feeds.npr.org/1001/rss.xml")
+		  ("WDWNT"		             "https://wdwnt.com/feed/")
+		  ("Bleeping Computer"       "https://www.bleepingcomputer.com/feed/")
+		  ("JElse's Blog"            "https://jlelse.blog/.atom")
+		  ("KDE Blog"                "https://pointieststick.com/feed/")
+		  ("Ars Technica"            "http://feeds.arstechnica.com/arstechnica/index/")
+		  ("Kev Quirk's Blog"        "https://kevq.uk/feed/")
+		  ("Oh Hello Ana"            "https://ohhelloana.blog/feed.xml")
+		  ("Planet Emacs"            "https://planet.emacslife.com/atom.xml")
+		  ("My Blog"                 "https://degruchy.org/feed.xml"))))
