210fa9eb3b634c94c7ffc6f133bfe9df42be8a1b
Nathan DeGruchy <nathan@degruchy.org>
2022-08-30 11:43:24 -0400

    Remove pre-defined height/width on regular pictures

--- skeletons.d/web.el
+++ skeletons.d/web.el
@@ -60,7 +60,7 @@
 	> "<picture>\n"
 	> "<source srcset=\"/assets/images/posts/" date "/\" type=\"image/heic\">\n"
 	> "<source srcset=\"/assets/images/posts/" date "/\" type=\"image/webp\">\n"
-	> "<img href=\"/assets/images/posts/" date "/\" alt=\"\" height=\"128\" width=\"128\" loading=\"lazy\">\n"
+	> "<img href=\"/assets/images/posts/" date "/\" alt=\"\" height=\"\" width=\"\" loading=\"lazy\">\n"
 	> "</picture>\n"
 	> "<figcaption>" _ "</figcaption>\n"
 	> "</figure>\n")
