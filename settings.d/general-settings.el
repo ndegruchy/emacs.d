a1d6b093c261e7929d32c1d0644410380ad2b220
Nathan DeGruchy <nathan@degruchy.org>
2022-09-08 13:02:19 -0400

    Updates

--- settings.d/general-settings.el
+++ settings.d/general-settings.el
@@ -137,3 +137,14 @@
 ;; Window moving keybindings
 (when (fboundp 'windmove-default-keybindings)
 	(windmove-default-keybindings))
+
+;; Disable customize functions
+(dolist (sym '(customize-option customize-browse customize-group customize-face
+				  customize-rogue customize-saved customize-apropos
+				  customize-changed customize-unsaved customize-variable
+				  customize-set-value customize-customized customize-set-variable
+				  customize-apropos-faces customize-save-variable
+				  customize-apropos-groups customize-apropos-options
+				  customize-changed-options customize-save-customized))
+	(put sym 'disabled "I've disabled `customize', configure Emacs from $XDG_CONFIG_HOME/emacs/config.el instead"))
+(put 'customize-themes 'disabled "I've disabled `customize', configure Emacs from $XDG_CONFIG_HOME/emacs/config.el instead")
