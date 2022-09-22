4d0bc1e41ae46b0d8ded6d07f93dd48edd6e1e66
Nathan DeGruchy <nathan@degruchy.org>
2022-09-22 12:52:06 -0400

    Added prompts for new item

--- skeletons.d/rec.el
+++ skeletons.d/rec.el
@@ -8,12 +8,16 @@
 (define-skeleton ndegruchy/skeleton-rec-new-contact
 	"Inserts a skeletal contact card record for a rec-mode database"
 	nil
+	'(setq gn (skeleton-read "Given Name: "))
+	'(setq fn (skeleton-read "Family Name: "))
+	'(setq ea (skeleton-read "Email: "))
+	'(setq ph (skeleton-read "Phone: "))
 	;; Should I just ask for these up front? Seems like an obvious choice
-	"Given_Name: " _ "\n"
-	"Family_Name: " _ " \n"
+	"Given_Name: " gn "\n"
+	"Family_Name: " fn " \n"
 	"Street_Address: " _ " \n"
-	"Email_Address: " _ " \n"
-	"Phone: " _ " \n")
+	"Email_Address: " ea " \n"
+	"Phone: " ph " \n")
 
 (define-skeleton ndegruchy/skeleton-rec-new-file
 	"Inserts a skeleton for new recutils file"
