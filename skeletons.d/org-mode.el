b06b0c29f7c352499387a50fbf969e7b15ec453e
Nathan DeGruchy <nathan@degruchy.org>
2022-10-06 12:52:43 -0400

    Add new lines... whoops

--- skeletons.d/org-mode.el
+++ skeletons.d/org-mode.el
@@ -3,8 +3,10 @@
 (define-skeleton ndegruchy/skeleton-org-new-file
 	"Inserts some basic 'front matter' into an org file"
 	"Title: "
-	"#+TITLE: " str
-	"#+AUTHOR: " (user-full-name)
-	"#+DATE: " (format-time-string "%Y-%m-%d")
-	"#+OPTIONS: toc:nil num:nil"
-	"#+Time-stamp: <>")
+	"#+TITLE: " str "\n"
+	"#+AUTHOR: " (user-full-name) "\n"
+	"#+DATE: " (format-time-string "%Y-%m-%d") "\n"
+	"#+OPTIONS: toc:nil num:nil\n"
+	"#+Time-stamp: <>\n"
+	"\n"
+	_)
