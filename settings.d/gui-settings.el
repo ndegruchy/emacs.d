bebf8fb188f5639cb4d205ba1afc4e0c2c22e320
Nathan DeGruchy <nathan@degruchy.org>
2022-10-18 16:29:21 -0400

    Documentation

--- settings.d/gui-settings.el
+++ settings.d/gui-settings.el
@@ -6,10 +6,9 @@
 (tool-bar-mode   -1)
 (menu-bar-mode   -1)
 
-(setq x-gtk-use-system-tooltips nil ;; I don't use GTK build anyway
-	  inhibit-x-resources       t   ;; I also want to customize this myself, thanks
-	  use-dialog-box 			nil ;; No UI dialogs, either
-	  )
+(setq x-gtk-use-system-tooltips nil  ;; I don't use GTK build anyway
+	  inhibit-x-resources       t    ;; I also want to customize this myself, thanks
+	  use-dialog-box 			nil) ;; No UI dialogs, either
 
 ;; Fonts
 (add-to-list 'default-frame-alist '(font . "Iosevka-17"))
--- settings.d/win32-settings.el
+++ settings.d/win32-settings.el
@@ -1,3 +1,14 @@
+;;; win32-settings.el --- A place for win32 platform settings  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Nathan DeGruchy
+
+;; Author: Nathan DeGruchy <nathan@degruchy.org>
+;; Keywords: local, processes, convenience
+
+;; Cascadia is more likely to be installed on Windows than Ioveska
 (add-to-list 'default-frame-alist '(font . "Cascadia Code-14"))
+
+;; Set my default directory to the root of the profile, not %APPDATA%
+;; And set some ls-lisp.el arguments
 (setq default-directory "C:/Users/NathanRDeGruchy/"
 	dired-listing-switches "-a -F -l")
