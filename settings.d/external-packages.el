793dfeab166d6811eef82a44065e30474f8a957e
Nathan DeGruchy <nathan@degruchy.org>
2022-10-27 09:58:44 -0400

    Add documentation, scrobbling and modline customizations

--- settings.d/external-packages.el
+++ settings.d/external-packages.el
@@ -45,26 +45,46 @@
 (require 'rec-mode) ;; Loaded from site-lisp.d
 
 ;; Emms
+;; Load all the bits I want
 (require 'emms-setup)
 (require 'emms-mode-line)
+(require 'emms-mode-line-icon)
 (require 'emms-player-vlc)
 (require 'emms-info-native)
+(require 'emms-librefm-scrobbler)
+
+;; Enable the functions
 (emms-all)
 (emms-mode-line 1)
+(emms-playing-time-disable-display)
+
+;; Settings
+(setq emms-mode-line-icon-enabled-p nil)
 (setq emms-info-functions '(emms-info-native))
 (setq emms-source-file-default-directory (concat (getenv "HOME") "/Media/Music"))
 (setq emms-info-asynchronosly t)
 (setq emms-show-format "%s")
 (setq emms-browser-covers 'emms-browser-cache-thumbnail-async)
+
+;; Use console-vlc, if available
 (if (executable-find "cvlc")
 	(setq emms-player-list '(emms-player-vlc))
   (emms-default-players))
+
+;; Libre.FM scrobbling
+(setq emms-librefm-scrobbler-username "ndegruchy"
+	  emms-librefm-scrobbler-password librefm-password)
+
+;; Control surfaces
 (global-set-key (kbd "C-c e b") 'emms-smart-browse)
 (global-set-key (kbd "C-c e p") 'emms-pause)
 (global-set-key (kbd "C-c e N") 'emms-next)
 (global-set-key (kbd "C-c e P") 'emms-previous)
 (global-set-key (kbd "C-c e s") 'emms-stop)
 (global-set-key (kbd "C-c e l") 'ndegruchy/emms-show-playlist)
+(global-set-key (kbd "C-c e L") 'emms-metaplaylist-mode-go) ;; Show all playlists
+
+;; Custom functions for emms
 (defun ndegruchy/emms-show-playlist ()
   "Shows *just* the playlist buffer without having to invoke some
 form of the EMMS browse functions"
@@ -73,6 +93,8 @@ form of the EMMS browse functions"
 (defun ndegruchy/emms-mode-hook ()
   "Customizations to various emms buffers and modes"
   (hl-line-mode 1))
+
+;; Emms hooks
 (add-hook 'emms-browser-mode-hook #'ndegruchy/emms-mode-hook)
 (add-hook 'emms-playlist-mode-hook #'ndegruchy/emms-mode-hook)
 
