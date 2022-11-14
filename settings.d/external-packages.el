404138dd1463c675cb38c918b87c0ac53a647d9a
Nathan DeGruchy <nathan@degruchy.org>
2022-11-14 14:49:53 -0500

    Remove melpa

--- settings.d/external-packages.el
+++ settings.d/external-packages.el
@@ -12,22 +12,14 @@
 
 (add-to-list 'package-archives
 			 '("elpa" . "https://elpa.gnu.org/packages/"))
-;; (add-to-list 'package-archives
-;; 			 '("stable" . "https://stable.melpa.org/packages/"))
 (add-to-list 'package-archives
 			 '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
-;; (add-to-list 'package-archives
-;; 			 '("melpa" . "https://melpa.org/packages/") t)
-;; (add-to-list 'package-archives
-;;			 '("melpa-stable-mirror" . "https://www.mirrorservice.org/sites/stable.melpa.org/packages/") t)
 
 ;; Package archive priorities
 ;; Higher number = picked first
 (setq package-archive-priorities
 	  '(("elpa"   . 25) ;; Make ELPA the highest priority
-		;;("stable" . 20) 
 		("nongnu" . 15))) ;; Non-GNU has some good stuff
-		;;("melpa" . 1))) ;; I don't care much for the unstable "latest" stuff
 
 ;; Fetch required packages
 
